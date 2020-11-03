88269c0cb44fe8195ddb52de53250a23702fb910
michelled <michelleddevop@gmail.com>
2020-11-03 11:35:49 -0800

    init

---
+++ db/mongoDetails.js
@@ -0,0 +1,1 @@
+"mongodb://brad123:brad123@tmcluster-shard-00-00.49zsn.mongodb.net:27017,tmcluster-shard-00-01.49zsn.mongodb.net:27017,tmcluster-shard-00-02.49zsn.mongodb.net:27017/test?ssl=true&replicaSet=atlas-a1od78-shard-0&authSource=admin&retryWrites=true";
